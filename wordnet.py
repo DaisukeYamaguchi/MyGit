4e5e4638e51990cf748cf5dbc482f4d8a46e916a
DaisukeYamaguchi <112223242dy@gmail.com>
2017-07-14 00:32:32 +0900

    Add files via upload

---
+++ wordnet.py
@@ -0,0 +1,78 @@
+# encoding: utf-8
+
+import sys
+import sqlite3
+from collections import namedtuple
+
+conn = sqlite3.connect("wnjpn.db")
+
+Word = namedtuple('Word', 'wordid lang lemma pron pos')
+
+def getWords(lemma):
+    cur = conn.execute("select * from word where lemma=?", (lemma,))
+    return [Word(*row) for row in cur]
+
+def getWord(wordid):
+    cur = conn.execute("select * from word where wordid=?", (wordid,))
+    return Word(*cur.fetchone())
+
+Sense = namedtuple('Sense', 'synset wordid lang rank lexid freq src')
+
+def getSenses(word):
+    cur = conn.execute("select * from sense where wordid=?", (word.wordid,))
+    return [Sense(*row) for row in cur]
+
+def getSense(synset, lang='jpn'):
+    cur = conn.execute("select * from sense where synset=? and lang=?",
+                      (synset,lang))
+    row = cur.fetchone()
+    return row and Sense(*row) or None
+
+Synset = namedtuple('Synset', 'synset pos name src')
+
+def getSynset(synset):
+    cur = conn.execute("select * from synset where synset=?", (synset,))
+    return Synset(*cur.fetchone())
+
+SynLink = namedtuple('SynLink', 'synset1 synset2 link src')
+
+def getSynLinks(sense, link):
+    cur = conn.execute("select * from synlink where synset1=? and link=?",
+                      (sense.synset, link))
+    return [SynLink(*row) for row in cur]
+
+def getSynLinksRecursive(senses, link, lang='jpn', _depth=0):
+    for sense in senses:
+        synLinks = getSynLinks(sense, link)
+        if synLinks:
+          print(''.join([' '*2*_depth,
+                         getWord(sense.wordid).lemma,
+                         ' ',
+                         getSynset(sense.synset).name]))
+        _senses = []
+        for synLink in synLinks:
+            sense = getSense(synLink.synset2, lang)
+            if sense:
+                _senses.append(sense)
+            
+        getSynLinksRecursive(_senses, link, lang, _depth+1)
+    
+
+def abstract_word(lemma):
+    result = []
+    for word in getWords(lemma):
+        for sense in getSenses(word):
+            if sense.src != 'hand': continue
+            for synlink in getSynLinks(sense, 'hype'):
+                abst_sense = getSense(synlink.synset2)
+                if abst_sense and word.wordid != abst_sense.wordid:
+                    result.append(getWord(abst_sense.wordid).lemma)
+    return result
+
+
+lemma = u"ヨーグルト"
+print(lemma)
+
+for abst_word in abstract_word(lemma=lemma):
+    print(abst_word)
+
