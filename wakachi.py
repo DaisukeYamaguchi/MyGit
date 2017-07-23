f872b79d5dc33f005297c086c37caa7abea035f2
DaisukeYamaguchi <112223242dy@gmail.com>
2017-07-23 16:02:12 +0900

    Add files via upload

--- wakachi.py
+++ wakachi.py
@@ -8,7 +8,7 @@ with open('all_ziburi_text.wkc', 'w', encoding = 'utf8') as f1:
     with open('all_ziburi_text.txt', 'r', encoding = 'utf8') as f2:
         for line in f2:
             wordlist = []
-            tokens = t.tokenize(line)
+            tokens = t.tokenize(line.rstrip('\n'))
             for token in tokens:
                 check = token.part_of_speech.split(',')
                 if check[0] == '名詞':
