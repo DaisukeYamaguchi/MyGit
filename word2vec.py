faf0a9ea94f8ccd47f5873a72e786ae7efb119c5
DaisukeYamaguchi <112223242dy@gmail.com>
2017-07-23 14:15:53 +0900

    Add files via upload

--- word2vec.py
+++ word2vec.py
@@ -1,21 +1,36 @@
 # -*- coding: utf-8 -*-
+import sqlite3
 from gensim.models import word2vec
-data = word2vec.Text8Corpus('yourname.txt')
-model = word2vec.Word2Vec(data, size=200)
 
-model.save("wakati.model")
+database = 'ziburi'
+dimension = 50
 
-out=model.most_similar(positive=[u'俺',u'私'],negative=[u'瀧'])
-for x in out:
-    print x[0],x[1]
+data = word2vec.Text8Corpus('all_ziburi_text.wkc')
+model = word2vec.Word2Vec(data, size = dimension)
 
+columns = 'word text'
+for i in range(dimension):
+    columns += ', var' + str(i+1) + ' real'
 
+query = ['?' for i in range(dimension + 1)]
 
-model = word2vec.Word2Vec.load('wakati.model')
+sql = 'INSERT INTO word2vec VALUES (' + ','.join(query) + ')'
 
-model.corpus_count
+word = list(model.vocab.keys())
+words = []
+for item1 in word:
+    temp = []
+    temp.append(item1)
+    for item2 in model[item1]:
+        temp.append(item2)
+    
+    words.append(temp)
 
-model.vocab
+conn = sqlite3.connect(database+'.db')
 
+c = conn.cursor()
 
+c.execute('''CREATE TABLE word2vec (''' + str(columns) + ''')''')
+
+c.executemany(sql, words)
 
